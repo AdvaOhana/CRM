f16849c947fe3acf0dcb30b7c31268c031231a93
advaohana <adva1230@gmail.com>
2026-01-02 10:58:44 +0200

    run

--- server/test/math.js
+++ server/test/math.js
@@ -1,16 +1,11 @@
-function sum(a, b) {
+export function sum(a, b) {
     return a + b;
 }
 
-function doul(a) {
+export function doul(a) {
     return a % 2 == 0;
 }
 
-function p(a, b) {
+export function p(a, b) {
     return a - b;
-}
-module.exports = {
-    sum,
-    doul,
-    p
 }
